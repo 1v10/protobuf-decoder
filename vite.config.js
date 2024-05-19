6b6cf752d38e66da13468f2a96bc523fbc6ccbd7
phelop <45528455+1v10@users.noreply.github.com>
2024-05-19 17:19:19 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    sourcemap: true
+    sourcemap: true,
+    outDir: 'dist'  // 添加这一行
   },
   plugins: [react()],
   test: {
